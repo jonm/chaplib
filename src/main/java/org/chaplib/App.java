67ce61d5da8225be07f5bda1d7ec7d2fd4d3b5df
Jon Moore <jonm@jjmoore.net>
2012-03-12 19:22:38 -0400

    Skeletal Maven project

---
+++ src/main/java/org/chaplib/App.java
@@ -0,0 +1,13 @@
+package org.chaplib;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
